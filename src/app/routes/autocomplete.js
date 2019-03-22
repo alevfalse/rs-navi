6ff20e4852fb5a860c29b856cd071ae8067b8270
alevfalse <exactfalse@gmail.com>
2019-03-22 15:26:57 +0800

    check if query's length is at least 3 + 404 redirect

--- src/app/routes/autocomplete.js
+++ src/app/routes/autocomplete.js
@@ -7,30 +7,43 @@ function regexEscape(str) {
 
 autocompleteRouter.get('/schools', (req, res) => {
     const query = regexEscape(req.query.query);
+
+    if (!query || query.length <= 2) {
+        return res.json({ suggestions: [] });
+    }
+
     const regex = new RegExp(query, 'i');
 
-    console.log(`Regex: ${regex}`);
+    process.nextTick(() => {
+        School.find({ 'name': { $regex : regex } }, { 'name': 1 }, (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.json({ suggestions: [] });
+            }
     
-    let schools = [];
-
-    School.find({ 'name': { $regex : regex } }, { 'name': 1 }, (err, result) => {
-        if (err) {
-            console.log(err);
-            return res.json(null);
-        }
-
-        result.forEach((school) => {
-            schools.push(school.name);
-        })
-
-        schools.sort();
-        
-        res.json({ suggestions: schools });
-    })
-})
+            if (result.length == 0) {
+                return res.json({ suggestions: [] });
+            }
+    
+            let schools = [];
+            
+            result.forEach((school) => {
+                schools.push(school.name);
+            })
+    
+            schools.sort();
+            
+            res.json({ suggestions: schools });
+        });
+    });
+});
 
 autocompleteRouter.get('/*', (req, res) => {
-    res.sendStatus(404);
-})
+    req.flash('message', 'Page not found.');
+    req.session.save((err) => {
+        if (err) { console.error(err); }
+        res.redirect('/');
+    })
+});
 
 module.exports = autocompleteRouter;
