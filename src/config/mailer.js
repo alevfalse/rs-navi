09b94dbd9df0d7b6540242d38e7ed36d67b41d9f
alevfalse <exactfalse@gmail.com>
2019-03-22 15:28:37 +0800

    removed env check

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,9 +1,5 @@
 const nodemailer = require('nodemailer');
 
-if (!process.env.RSNAVI_GMAIL_PASSWORD) {
-    return console.error(`No RSNAVI_GMAIL_PASSWORD found. Check your .env file.`);
-}
-
 const gmail = nodemailer.createTransport({
     service: 'gmail',
     auth: {
