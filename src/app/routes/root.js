3898495fc9d43200f60cdafd842acfbc380eadaa
alevfalse <exactfalse@gmail.com>
2019-03-22 15:27:51 +0800

    save session after setting flash message + status 500

--- src/app/routes/root.js
+++ src/app/routes/root.js
@@ -2,41 +2,32 @@ const openRouter = require('express').Router();
 const request = require('request-promise');
 // const argon2 = require('argon2');
 
-const mongoose = require('mongoose');
 const Query = require('../models/query');
 
+// a middle to check if a user is logged in
 function isAuthenticated(req, res, next) {
+    console.log(`${req.url}: Authenticated: ${req.isAuthenticated()}`);
     if (req.isAuthenticated()) {
-        next();
+        return next();
     } else {
-        console.log(`Unauthorized access for ${req.url}`)
-        res.redirect('/');
+        req.flash('message', 'Unauthorized!');
+        req.session.save((err) => {
+            if (err) console.error(err);
+            res.redirect('/auth');
+        })
     }
 }
 
 openRouter.get('/', (req, res) => {
-    console.log(`Authenticated: ${req.isAuthenticated()}`);
-
-    const flashMessage = req.flash('message');
-    console.log(`Flash Message: ${flashMessage}`);
-
-    res.render('index', { user: req.user, message: flashMessage }, (err, html) => {
-        if (err) {
-            console.error(`Something went wrong while rendering the [index] page:\n${err}`);
-            res.sendStatus(500);
+    res.render('index', { user: req.user, message: req.flash('message') },
+        (err, html) => {
+            if (err) { 
+                console.error(err);
+                return res.sendStatus(500); 
+            }
+            res.send(html);
         }
-        res.status(200).send(html);
-    });
-})
-
-openRouter.get('/profile', isAuthenticated, (req, res) => {
-    res.render('profile', { user: req.user, message: req.flash('message') }, (err, html) => {
-        if (err) {
-            console.error(`Something went wrong while rendering the [profile] page:\n${err}`);
-            res.sendStatus(500);
-        }
-        res.status(200).send(html); 
-    });
+    );
 })
 
 openRouter.get('/search', (req, res) => {
@@ -44,40 +35,53 @@ openRouter.get('/search', (req, res) => {
 
     if (!query) {
         req.flash('message', 'Please provide a school name.');
-        res.redirect('/');
+        req.session.save((err) => {
+            if (err) console.error(err);
+            res.redirect('/');
+        });
+        return;
+    } else if (query.length <= 1) {
+        req.flash('message', 'School name is too short.');
+        req.session.save((err) => {
+            if (err) console.error(err);
+            res.redirect('/');
+        });
+        return;
     }
-    console.log(`Search Query: ${query}`);
 
     const newQuery = new Query({ keyword: query });
     newQuery.save((err, savedQuery) => {
-        if (err) {
-            console.error(`An error occurred while saving query [${query}] to database:\n${err}`);
-        } else {
-            console.log(`Search query "${savedQuery.keyword}" has been saved to the database.`);
-        }
+        if (err) console.error(`An error occurred while saving query [${query}] to database:\n${err}`);
+        else console.log(`Search query "${savedQuery.keyword}" has been saved to the database.`);
     })
 
-    if (process.env.MAPBOX_ACCESS_TOKEN) {
-        const uri = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json`
-        + `?access_token=${process.env.MAPBOX_ACCESS_TOKEN}&autocomplete=true&country=ph`;
+    const uri = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json`
+    + `?access_token=${process.env.MAPBOX_ACCESS_TOKEN}&autocomplete=true&country=ph`;
 
-        console.log(uri);
+    request(uri).then(response => {
+        const data = JSON.parse(response);
+        res.send(data.features); // update for user friendly format
+    }).catch(err => console.error(err));
+})
 
-        request(uri).then(response => {
-            const data = JSON.parse(response);
-            //console.log(data.features);
-            res.send(data.features);
-        }).catch(err => console.error(err));
-
-    } else {
-        console.error('No MAPBOX ACCESS TOKEN in your .env file.');
-        req.flash('message', 'Search is temporarily unavailable at the moment. Please try again later.');
-        res.status(503).redirect('/');
-    }
+openRouter.get('/profile', isAuthenticated, (req, res) => {
+    res.render('profile', { user: req.user },
+        (err, html) => {
+            if (err) { 
+                console.error(err);
+                return res.sendStatus(500); 
+            }
+            res.send(html);
+        }
+    );
 })
 
 openRouter.get('/*', (req, res) => {
-    res.sendStatus(404);
+    req.flash('message', 'Page not found.');
+    req.session.save((err) => {
+        if (err) console.error(err);
+        res.redirect('/');
+    })
 })
 
 module.exports = openRouter;
