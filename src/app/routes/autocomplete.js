4e5f2ed217609d6994fe89120ea5b28f3f57ce15
alevfalse <exactfalse@gmail.com>
2019-03-22 19:11:55 +0800

    added query projection to return school name only

--- src/app/routes/autocomplete.js
+++ src/app/routes/autocomplete.js
@@ -8,21 +8,22 @@ function regexEscape(str) {
 autocompleteRouter.get('/schools', (req, res) => {
     const query = regexEscape(req.query.query);
 
-    if (!query || query.length <= 2) {
-        return res.json({ suggestions: [] });
+    if (!query || query.length <= 3) {
+        return res.json({ suggestions: [''] });
     }
 
     const regex = new RegExp(query, 'i');
 
     process.nextTick(() => {
-        School.find({ 'name': { $regex : regex } }, { 'name': 1 }, (err, result) => {
+        School.find({ 'name': { $regex : regex } }, { 'name': 1, '_id': 0 }, (err, result) => {
+
             if (err) {
                 console.log(err);
-                return res.json({ suggestions: [] });
+                return res.json({ suggestions: [''] });
             }
     
             if (result.length == 0) {
-                return res.json({ suggestions: [] });
+                return res.json({ suggestions: [''] });
             }
     
             let schools = [];
