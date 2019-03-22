c9275c9db899f6a0bda1b3d4655a8c618d664fc9
alevfalse <exactfalse@gmail.com>
2019-03-23 00:54:16 +0800

    embedded reviews into place document instead of reference

--- src/app/models/place.js
+++ src/app/models/place.js
@@ -2,13 +2,24 @@ const mongoose = require('mongoose');
 
 const PlaceSchema = new mongoose.Schema({
     owner: { type: mongoose.Schema.Types.ObjectId, ref: 'Placeowner' },
-    reviews: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Review'} ],
+    reviews: [
+        {
+            author: { type: mongoose.Schema.Types.ObjectId, ref: 'Student' },
+            rating: Number,
+            comment: String,
+            status: { type: Number, default: 0 },
+            votes:  { type: Number, default: 0 },
+            date:   { type: Date, default: new Date() },
+            edited: { type: Boolean, default: false }
+        }
+    ],
     status: { type: Number, default: 0 }
 })
+
 /* status:
 0 - good
-1 - flagged
-3 - deleted
+1 - bad
+2 - deleted
 */
 
 module.exports = mongoose.model('Place', PlaceSchema);
