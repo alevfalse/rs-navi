0f862e757cf8089d73884a7d5a9d2f19c2665389
alevfalse <exactfalse@gmail.com>
2019-03-26 03:13:07 +0800

    added page not found and some route comments

--- src/app/routes/places.js
+++ src/app/routes/places.js
@@ -1,12 +1,20 @@
 const placesRouter = require('express').Router();
 const upload = require('../../config/upload');
 
+// GET rsnavigation.com/places/add
 placesRouter.get('/add', (req, res) => {
-    res.render('add-place');
+    return res.render('add-place');
 });
 
+// GET rsnavigation.com/places/add
+placesRouter.get('/*', (req, res) => {
+    req.flash('message', 'Page not found.');
+    return res.redirect('/places/add');
+});
+
+// POST rsnavigation.com/places/add
 placesRouter.post('/add', upload.single('file'), (req, res) => {
-    res.json({ file: req.file });
+    return res.json({ file: req.file });
 });
 
 module.exports = placesRouter;
