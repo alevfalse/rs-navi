131240f91ec9047150618d9ec9c4569eb34b2244
alevfalse <exactfalse@gmail.com>
2019-03-26 03:10:15 +0800

    added cookie domain, log files, installed rotating file stream

--- src/server.js
+++ src/server.js
@@ -1,59 +1,28 @@
 console.log('Configuring application...');
-require('dotenv').config();
-
-if (!require('./check.env')()) {
-    process.exit(1);
-}
 
 // ENVIRONMENT VARIABLES
+require('dotenv').config();
+if (!require('./check.env')()) { process.exit(1); }
 const port = process.env.PORT || 5000;
 const mode = process.env.MODE;
 
 // REQUIRED MODULES ======================================================================
+const path       = require('path');
+const rfs        = require('rotating-file-stream');
+const fs         = require('fs');
 const express    = require('express');
 const session    = require('express-session');
-const mongoose   = require('mongoose');
-const morgan     = require('morgan');
-const bodyParser = require('body-parser');
-const MongoStore = require('connect-mongo')(session);
 const flash      = require('connect-flash');
+const bodyParser = require('body-parser');
+const mongoose   = require('mongoose');
+const MongoStore = require('connect-mongo')(session);
+const morgan     = require('morgan');
 
 // AUTHENTHICATION =======================================================================
 const passport   = require('./config/passport');
 
-// DATABASE ==============================================================================
-const database   = require('./config/database');
-
-// APPLICATION ===========================================================================
-const app = express();
-
-app.set('view engine', 'ejs'); // templating engine for dynamic pages
-app.set('views', __dirname + '/views'); // folder containing the pages to be served
-
-app.use(express.static(__dirname + '/public')); // serving static files
-app.use(morgan(process.env.MODE == 'prod' ? 'combined' : 'dev')); // logging
-
-// for parsing request body
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// SESSION ===============================================================================
-app.use(session({
-    name: 'rs-navi.session',
-    secret: process.env.SESSION_SECRET,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 * 3 }, // max age of 3 days
-    saveUninitialized: false,   // don't save session in the database if not modified
-    resave: false,              // don't save session if unmodified
-    store: new MongoStore({ 
-        mongooseConnection: mongoose.connection,
-        touchAfter: 60 * 60 * 24 // the session will be updated only one time in a period of 24 hours, 
-        // does not matter how many request's are made (with the exception of those that change something on the session data)
-    })
-}))
-
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash()); // for flashing messages between requests
+// DATABASE CONNECTION ===================================================================
+const connection = require('./config/connection');
 
 // ROUTERS ===============================================================================
 const authRouter = require('./app/routes/auth');
@@ -62,6 +31,47 @@ const autocompleteRouter = require('./app/routes/autocomplete');
 const placesRouter = require('./app/routes/places');
 const adminRouter = require('./app/routes/admin');
 const rootRouter = require('./app/routes/root');
+
+// APPLICATION ===========================================================================
+const app = express();
+
+if (mode === 'prod') { app.enable('trust proxy'); }
+
+app.use(express.static(__dirname + '/public')); // serving static files
+
+// logging
+const logDirectory = path.join(__dirname + '/logs');
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory); // create logs folder if it does not exist
+app.use(morgan('dev'));
+app.use(morgan('common', { stream: fs.createWriteStream(path.join(logDirectory, 'access.log'), { flags: 'a' }) }));
+
+// for parsing request body
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs'); // templating engine for dynamic pages
+app.set('views', __dirname + '/views'); // folder containing the pages to be served
+
+
+// session
+const cookie = { maxAge: 1000 * 60 * 60 * 24 * 3 };
+if (mode === 'prod') cookie.domain = '.rsnavigation.com';
+app.use(session({
+    name: 'rs-navi.session',
+    secret: process.env.SESSION_SECRET,
+    cookie: cookie,
+    saveUninitialized: true,   // don't save session in the database if not modified
+    resave: true,              // don't save session if unmodified
+    store: new MongoStore({ 
+        mongooseConnection: mongoose.connection
+        //touchAfter: 60 * 60 * 24 // the session will be updated only one time in a period of 24 hours, 
+        // does not matter how many request's are made (with the exception of those that change something on the session data)
+    })
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash()); // for flashing messages between requests
 
 // bind the routes to the application
 app.use('/auth', authRouter);
@@ -73,20 +83,9 @@ app.use('/', rootRouter);
 
 console.log('Application configured.');
 
-let uri;
-switch (mode)
-{
-case 'dev':   uri = database.devURI;   break;
-case 'prod':  uri = database.prodURI;  break;
-case 'local': uri = database.localURI; break;
-default: return console.error('Invalid MODE environment variable value.');
-}
-
-console.log(`Connecting to ${mode} database...`);
-mongoose.connect(uri, { useNewUrlParser: true }, (err) => {
-    if (err) { return console.error(`An error occurred while trying to connect to the ${mode} database:\n${err}`); }
+connection.once('connected', () => {
     console.log(`Application successfully connected to ${mode} database.`);
     app.listen(port, () => {
-        console.log(`${mode === 'prod' ? 'rsnavigation.com' : 'Application'} is now live and listening to port ${port}!`);
+        console.log(`${mode === 'prod' ? 'rsnavigation.com is now live and' : 'RS Navi (Dev) is now '} listening to port ${port}!`);
     });
-});
+})
