b07e830bc54cb924d0da1a7fd1883e404c958036
alevfalse <exactfalse@gmail.com>
2019-03-27 17:21:53 +0800

    admin homepage

--- src/app/routes/admin.js
+++ src/app/routes/admin.js
@@ -1,6 +1,10 @@
 const adminRouter = require('express').Router();
 const passport = require('../../config/passport');
 
+const Student = require('../models/student');
+const Placeowner = require('../models/placeowner');
+
+
 function isAuthenticated(req, res, next) {
     if (req.isAuthenticated() && req.user.account.role == 7) {
         return next();
@@ -14,19 +18,33 @@ function isAuthenticated(req, res, next) {
 // GET rsnavigation.com/admin
 adminRouter.get('/', isAuthenticated, (req, res) => {
     
-    req.session.save((err) => {
+    Student.find((err, students) => {
         if (err) {
             console.error(err);
             return res.sendStatus(500); // TODO: Send custom status 500 page
         }
 
-        res.render('admin/home', { message: req.flash('message') },
-        (err, html) => {
+        Placeowner.find((err, placeowners) => {
             if (err) {
                 console.error(err);
                 return res.sendStatus(500); // TODO: Send custom status 500 page
             }
-            return res.send(html);
+
+            req.session.save((err) => {
+                if (err) {
+                    console.error(err);
+                    return res.sendStatus(500); // TODO: Send custom status 500 page
+                }
+        
+                res.render('admin/home', { admin: req.user, students: students, placeowners: placeowners, message: req.flash('message') },
+                (err, html) => {
+                    if (err) {
+                        console.error(err);
+                        return res.sendStatus(500); // TODO: Send custom status 500 page
+                    }
+                    return res.send(html);
+                });
+            });
         });
     });
 });
