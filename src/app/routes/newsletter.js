33fd50fd07db495c8837dca0ce03ffb8695ebdd9
alevfalse <exactfalse@gmail.com>
2019-04-13 01:16:11 +0800

    now checks if already subscribed

--- src/app/routes/newsletter.js
+++ src/app/routes/newsletter.js
@@ -3,25 +3,32 @@ const Subscriber = require('../models/subscriber');
 
 newsletterRouter.post('/', (req, res, next) => {
 
-    // TODO: Check if already subscribed
-    const newSubscriber = new Subscriber({ email: req.body.email });
+    process.nextTick(() => {
 
-    newSubscriber.save((err) => {
-
-        if (err) { return next(err); }
-
-        req.flash('message', 'Successfully subscribed to newsletter.');
-
-        req.session.save((err) => {
+        Subscriber.findOne({ 'email': req.body.email }, '_id',
+        (err, subscriber) => {
             if (err) { return next(err); }
-
-            Subscriber.find((err, subscribers) => {
-                if (err) { return next(err); }
-
-                console.log(subscribers);
-                res.redirect('/');
-            });
+    
+            if (subscriber) { 
+                req.flash('message', 'Your email address is already subscribed.');
+                req.session.save((err) => {
+                    if (err) { return next(err); }
+                    res.redirect('/');
+                });
+    
+            } else {
+                const newSubscriber = new Subscriber({ email: req.body.email });
+    
+                newSubscriber.save((err) => {
+                    if (err) { return next(err); }
             
+                    req.flash('message', 'Successfully subscribed to newsletter.');
+                    req.session.save((err) => {
+                        if (err) { return next(err); }
+                        res.redirect('/');
+                    });
+                });
+            }
         });
     });
 });
