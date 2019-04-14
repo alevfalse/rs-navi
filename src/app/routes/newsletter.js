889094fdf555329cb15c4c96643d661c5dcdc22d
alevfalse <exactfalse@gmail.com>
2019-04-15 02:38:32 +0800

    removed nextTick

--- src/app/routes/newsletter.js
+++ src/app/routes/newsletter.js
@@ -5,33 +5,30 @@ newsletterRouter.post('/', (req, res, next) => {
 
     //TODO: validate and sanitize email address
 
-    process.nextTick(() => {
+    Subscriber.findOne({ 'email': req.body.email }, '_id',
+    (err, subscriber) => {
+        if (err) { return next(err); }
 
-        Subscriber.findOne({ 'email': req.body.email }, '_id',
-        (err, subscriber) => {
-            if (err) { return next(err); }
-    
-            if (subscriber) { 
-                req.flash('message', 'Your email address is already subscribed.');
+        if (subscriber) { 
+            req.flash('message', 'Your email address is already subscribed.');
+            req.session.save((err) => {
+                if (err) { return next(err); }
+                res.redirect('/');
+            });
+
+        } else {
+            const newSubscriber = new Subscriber({ email: req.body.email });
+
+            newSubscriber.save((err) => {
+                if (err) { return next(err); }
+        
+                req.flash('message', 'Successfully subscribed to newsletter.');
                 req.session.save((err) => {
                     if (err) { return next(err); }
                     res.redirect('/');
                 });
-    
-            } else {
-                const newSubscriber = new Subscriber({ email: req.body.email });
-    
-                newSubscriber.save((err) => {
-                    if (err) { return next(err); }
-            
-                    req.flash('message', 'Successfully subscribed to newsletter.');
-                    req.session.save((err) => {
-                        if (err) { return next(err); }
-                        res.redirect('/');
-                    });
-                });
-            }
-        });
+            });
+        }
     });
 });
 
