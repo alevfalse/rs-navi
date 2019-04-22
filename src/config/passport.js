7b4fa334b26c8604f5d217441bdfbf21a8def034
alevfalse <exactfalse@gmail.com>
2019-04-22 14:39:14 +0800

    moved signup validation, utilize account model

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,69 +1,21 @@
 const passport = require('passport')
 const LocalStrategy  = require('passport-local').Strategy;
+const argon = require('argon2');
+
+const generate = require('nanoid/generate');
+const alpha = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
 // Models
 const Student = require('../app/models/student');
 const Placeowner = require('../app/models/placeowner');
 const Admin = require('../app/models/admin');
+const Account = require('../app/models/account');
 
-// ===================================================================================================
-// FUNCTIONS =========================================================================================
+// mailer
+const sendVerificationLinkEmail = require('../bin/verification-link-email');
 
 // additional validation on top of the client-side validation
-function validateSignupForm(firstName, lastName, email, password, confirmPassword) {
-    
-    let errorMessage = '';
-
-    // first name
-    if (firstName.startsWith(' ')) {
-        errorMessage += 'First name must not start with a space.\n';
-    } else if (firstName.match(/[^a-zA-Zñ\s]|\s{2,}/) || firstName.match(/[^a-zA-Zñ\s]|\s{2,}/)) {
-        errorMessage += 'First name contains an invalid character.\n';
-    } else if (firstName.length == 0) {
-        errorMessage += 'First name cannot be empty.\n';
-    } else if (firstName.length > 50) {
-        errorMessage += 'First name must not be more than 50 characters.\n';
-    }
-
-    // last name
-    if (lastName.startsWith(' ')) {
-        errorMessage += 'Last name must not start with a space.\n';
-    } else if (lastName.match(/[^a-zA-Zñ\s]|\s{2,}/) || lastName.match(/[^a-zA-Zñ\s]|\s{2,}/)) {
-        errorMessage += 'Last name contains an invalid character.\n';
-    } else if (lastName.length == 0) {
-        errorMessage += 'Last name cannot be empty.\n';
-    } else if (lastName.length > 50) {
-        errorMessage += 'Last name must not be more than 50 characters.\n';
-    }
-    
-    // email
-    if (email.startsWith(' ')) {
-        errorMessage += 'Email address must not start with space.\n';
-    } else if (email.match(/[^a-zA-Z0-9.@_]/)) {
-        errorMessage += 'Email address contains an invalid character.\n';
-    } else if (email.length == 0) {
-        errorMessage += 'Email address cannot be empty.\n';
-    } else if (email.length > 50) {
-        errorMessage += 'Email address must not be more than 50 characters.\n';
-    } // regex: if email starts/ends with @ or period || 2 or more @ || an @ is preceded by a period
-    else if (!email.includes('@') || email.match(/[@.]$|^[@.]|@[^@]*@|\.@/)) {
-        errorMessage += 'Invalid email address.\n';
-    }
-
-    // password
-    if (password.length <= 7) {
-        errorMessage += 'Password must be at least 8 characters.\n';
-    } else if (password !== confirmPassword) {
-        errorMessage += 'Passwords do not match.\n';
-    }
-
-    // check if an error occurred
-    if (errorMessage.length > 0) {
-        return new Error(errorMessage);
-    } else {
-        return null;
-    }
-}
+const validateSignupForm = require('../bin/validate-signup-form');
 
 // stores the user's id in session to be used during deserialization
 passport.serializeUser((user, done) => {
@@ -81,7 +33,7 @@ passport.deserializeUser((id, done) => {
         case '7': query = Admin.findById(id);      break;
         default : return done(null, false);
     }
-    query.populate('image').exec((err, user) => done(err, user));
+    query.populate('image').populate('account').exec((err, user) => done(err, user));
 });
 
 
@@ -95,76 +47,47 @@ passport.use('local-login', new LocalStrategy({
     passwordField: 'password',
     passReqToCallback: true
 
-}, function(req, email, password, done) {
+}, async function(req, email, password, done) {
 
-    const role = req.body.role;
-    if (!email || !password || !role) { return done(null, false); }
+    const roleString = req.body.role;
 
-    switch (role.toLowerCase())
+    if (!email || !password || !roleString) { return done(null, false); }
+
+    let role;
+
+    switch(roleString)
     {
-    case 'student': {
-        Student.findOne({ 'account.email': email }, (err, student) => {
-            if (err) { return done(err, false); }
+        case 'student': role = 0; break;
+        case 'placeowner': role = 1; break;
 
-            if (!student || password !== student.account.password) {
-                req.flash('message', 'Invalid email or password.');
-                return done(null, false);
-            }
-
-            if (student.account.status === 0) {
-                req.flash('message', 'Your email address is still unverified.\n Please check your email for verification link.')
-                return done(null, false);
-            } else if (student.account.status === 2) {
-                req.flash('message', 'Your account has been temporarily suspended.')
-                return done(null, false);
-            } else if (student.account.status === 3) {
-                req.flash('message', 'Your account has been permanently banned.')
-                return done(null, false);
-            }
-
-            student.account.lastLoggedIn = new Date();
-            student.account.hashCode = null;
-            student.save((err) => {
-                if (err) { return done(err, false); }
-                return done(null, student);
-            })
-            
-        })
-    } break;
-
-    case 'placeowner': {
-        Placeowner.findOne({ 'account.email': email }, (err, placeowner) => {
-            if (err) { return done(err, false); }
-
-            if (!placeowner || password !== placeowner.account.password) {
-                req.flash('message', 'Invalid email or password.');
-                return done(null, false);
-            }
-
-            if (placeowner.account.status === 0) {
-                req.flash('message', 'Your email address is still unverified. Please check your email for verification link.')
-                return done(null, false);
-            } else if (placeowner.account.status === 2) {
-                req.flash('message', 'Your account has been temporarily suspended.')
-                return done(null, false);
-            } else if (placeowner.account.status === 3) {
-                req.flash('message', 'Your account has been permanently banned.')
-                return done(null, false);
-            }
-
-            placeowner.account.lastLoggedIn = new Date();
-            placeowner.account.hashCode = null;
-            placeowner.save((err) => {
-                if (err) { return done(err, false); }
-                return done(null, placeowner);
-            })
-        })
-    } break;
-
-    default:
-        req.flash('message', `Invalid role provided.`);
-        return done(null, false);
+        default: 
+            req.flash('message', 'Invalid role.');
+            return done(null, false);
     }
+    Account.findOne({ 'email': email, 'role': role },
+    async (err, account) => {
+        if (err) { return done(err, false); }
+
+        console.log(account);
+
+        if (!account || !await account.verifyPassword(password)) {
+            req.flash('message', 'Invalid email or password.');
+            return done(null, false);
+        }
+
+        if (account.status === 0) {
+            req.flash('message', 'Your email address is still unverified.\n Please check your email for verification link.')
+            return done(null, false);
+        } else if (account.status === 2) {
+            req.flash('message', 'Your account has been temporarily suspended.')
+            return done(null, false);
+        } else if (account.status === 3) {
+            req.flash('message', 'Your account has been permanently banned.')
+            return done(null, false);
+        }
+
+        account.login(err => err ? done(err, false) : done(null, account));
+    });
 }));
 
 // =====================================================================================================
@@ -174,16 +97,16 @@ passport.use('local-signup', new LocalStrategy({
     passwordField: 'password',
     passReqToCallback: true
 
-}, function(req, email, password, done) {
-    console.log('Local Signup Strategy Invoked');
+}, async function(req, email, password, done) {
+    console.log('--- Local Signup Strategy ---');
     console.log(req.body);
 
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
     const confirmPassword = req.body.confirmPassword;
-    const role = req.body.role;
+    const roleString = req.body.role;
     
-    if (!email || !firstName || !lastName || !confirmPassword || !role) {
+    if (!email || !firstName || !lastName || !confirmPassword || !roleString) {
         req.flash('message', `Missing required signup field(s).`);
         return done(null, false);
     }
@@ -194,70 +117,49 @@ passport.use('local-signup', new LocalStrategy({
         return done(null, false);
     }
 
-    switch (role.toLowerCase())
-    {
-    case 'student': {
-        const schoolName = req.body.schoolName;
+    let role = roleString === 'student' ? 0 : 'placeowner' ? 1 : null;
+    if (role === null) { 
+        req.flash('message', 'Invalid role.');
+        return done(null, false);
+    }
 
-        if (!schoolName) {
-            req.flash('message', `Missing school name.`);
+    Account.findOne({ 'email': email, 'role': role }, '_id', async (err, account) => {
+        if (err) { return done(err, false); }
+
+        if (account) { 
+            req.flash('message', 'Email already exists.');
             return done(null, false);
         }
 
-        Student.findOne({ 'account.email': email }, { 'id': 1 }, (err, student) => {
+        const data = {
+            firstName: firstName,
+            lastName: lastName,
+            schoolName: req.body.schoolName
+        }
+
+        const newUser = role === 0 ? new Student(data) : new Placeowner(data);
+        newUser.account = newUser._id;
+
+        const hashedPassword = await argon.hash(password, { timeCost: 50 });
+
+        const newAccount = new Account({
+            _id: newUser._id,
+            email: email,
+            password: hashedPassword,
+            hashCode: generate(alpha, 6)
+        });
+
+        newAccount.save(err => { if (err) { return done(err, false); }
+
+        newUser.save(err => { if (err) { return done(err, false); }
+
+        sendVerificationLinkEmail(newUser._id, newAccount, err => {
             if (err) { return done(err, false); }
 
-            if (student) {
-                req.flash('message', 'Student email already exists.');
-                return done(null, false);
-            }
-
-            const newStudent = new Student({
-                firstName: firstName,
-                lastName: lastName,
-                schoolName: schoolName,
-                account: { 
-                    email: email,
-                    password: password
-                }
-            })
-
-            newStudent.save((err) => {
-                if (err) { return done(err, false); }
-                return done(null, newStudent);
-            });
-        });
-    } break;
-
-    case 'placeowner': {
-        Placeowner.findOne({ 'account.email': email }, { 'id': 1 }, (err, placeowner) => {
-            if (err) { return done(err, null); }
-
-            if (placeowner) {
-                req.flash('message', 'Placeowner email already exists.');
-                return done(null, false);
-            }
-
-            const newPlaceowner = new Placeowner({
-                firstName: firstName,
-                lastName: lastName,
-                account: { 
-                    email: email,
-                    password: password
-                }
-            })
-
-            newPlaceowner.save((err) => {
-                if (err) { return done(err, false); }
-                return done(null, newPlaceowner);
-            });
-        });
-    } break;
-    
-    default:
-        req.flash('message', `Invalid role provided.`);
-        return done(null, false);
-    }
+            req.flash('message', 'Verification email sent.');
+            return done(null, newUser);
+        }); }); });
+    });
 }))
 
 // =====================================================================================================
