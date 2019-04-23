c57e6b028669b692373f0b74da8c43998250c389
alevfalse <exactfalse@gmail.com>
2019-04-24 02:41:57 +0800

    log route to server logs file

--- src/app/routes/root.js
+++ src/app/routes/root.js
@@ -1,6 +1,11 @@
 const rootRouter = require('express').Router();
 const Place = require('../models/place');
 
+const fs = require('fs');
+const path = require('path');
+
+const logsDirectory = path.join(__dirname, '../../logs/');
+
 rootRouter.get('/', (req, res, next) => {
     res.render('index', { user: req.user, message: req.flash('message') },
     (err, html) => err ? next(err) : res.send(html));
@@ -28,5 +33,13 @@ rootRouter.get('/search', (req, res, next) => {
     // TODO: Only send nearby places (same city?)
     Place.find((err, places) => err ? res.send(null) : res.send(places));
 });
+
+rootRouter.get('/logs', (req, res, next) => {
+    if (fs.existsSync(logsDirectory + 'access.log')) {
+        res.sendFile('access.log', { root: logsDirectory});
+    } else {
+        return next();
+    }
+})
 
 module.exports = rootRouter;
