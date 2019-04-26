9c4b764e46d1df9a13e6e13ec42a281090418b63
alevfalse <exactfalse@gmail.com>
2019-04-27 02:35:49 +0800

    default length of 10

--- src/bin/nanoid.js
+++ src/bin/nanoid.js
@@ -2,9 +2,9 @@ const generate = require('nanoid/generate');
 const alpha = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
 /**
- * @param { Number } length
+ * @param { Number } length - the length of the id, defaults to 10
  * @description Generate a random string with the specified length
  */
-module.exports = function(length) {
+module.exports = function(length=10) {
     return generate(alpha, length);
 }
