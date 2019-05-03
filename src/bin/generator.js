4b4c668fc02887276d9cec3498d980cf6dcbff41
alevfalse <exactfalse@gmail.com>
2019-05-04 00:33:13 +0800

    use Date.now() for first 8 characters of id

--- src/bin/generator.js
+++ src/bin/generator.js
@@ -2,9 +2,8 @@ const generate = require('nanoid/generate');
 const alpha = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
 /**
- * @param { Number } length - the length of the id, defaults to 10
  * @description Generate a random string with the specified length
  */
-module.exports = function(length=10) {
-    return generate(alpha, length);
+module.exports = function() {
+    return Date.now().toString(36) + generate(alpha, 8);
 }
