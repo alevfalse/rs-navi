8206b659041d571f2fbee07d78b91da44b7a59c5
alevfalse <exactfalse@gmail.com>
2019-05-04 00:25:42 +0800

    combined to mailer module

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,4 +1,5 @@
 const nodemailer = require('nodemailer');
+const logger = require('../config/logger');
 
 const gmail = nodemailer.createTransport({
     service: 'gmail',
@@ -8,4 +9,68 @@ const gmail = nodemailer.createTransport({
     }
 });
 
-module.exports = gmail;
+exports.sendErrorEmail = function(err, recepient='alexanderpaul.marinas@gmail.com', callback) {
+    const mailOptions = {
+        from: "roomstayin.navigation@gmail.com",
+        to: recepient,
+        subject: `RS Navigation Error: ${err.name}`,
+        text: err.message + '\n\n' + err.stack 
+    };
+
+    gmail.sendMail(mailOptions, (err, info) => {
+        if (err) { return callback(err); }
+        logger.info(`Error email sent to [${mailOptions.to}] - [${err.name}] - ${info.response}`);
+    });
+}
+
+exports.sendVerificationEmail = function(user, callback) {
+
+    let url = `${process.env.MODE === 'prod' ? 'https://rsnavigation.com' : `localhost:${process.env.PORT}`}`
+            + `/auth/verify/${user.account.hashCode}`;
+
+    const message = `Congratulations! You have successfully created an RS Navigation ${user.roleString} account and `
+        + `you are just one step away from acessing it.\n\n`
+        + `You can click this link to verify that this is indeed your email address:\n${url}\n\n`
+        + `If you do not remember signing up to https://rsnavigation.com, calm down. Don't panic.\n`
+        + `Just ignore this email and we will handle the rest.\n\n`
+        + `- RS Navi Team`;
+
+    const mailOptions = {
+        from: "roomstayin.navigation@gmail.com",
+        to: user.account.email,
+        subject: "RS Navigation - Email Verification",
+        text: message
+    };
+    
+    gmail.sendMail(mailOptions, (err, info) => {
+        if (err) { return callback(err); }
+        logger.info(`Verification email sent to [${user.account.email}] - ${info.response}`);
+        callback(null);
+    });
+}
+
+exports.sendResetPasswordEmail = function(account, callback) {
+
+    let url = `${process.env.MODE === 'prod' ? 'https://rsnavigation.com' : `localhost:${process.env.PORT}`}`
+            + `/auth/reset/${account._id}/${account.hashCode}`;
+
+    const message = `Good day! We have received a password reset request from your ${account.roleString} account.\n\n`
+        + `You can click this link to reset your password:\n${url}\n\n`
+        + `If you did not send this request, we suggest you secure your account by clicking this link anyway or requesting a new one\n`
+        + `at https://rsnavigation.com/auth and select Forgot Password. You may also update your password on your account's profile page\n`
+        + `if you still have access to it (which we hope that you do). \n\n`
+        + `Thank you for using RS Navi.\n- RS Navi Team`;
+
+    const mailOptions = {
+        from: "roomstayin.navigation@gmail.com",
+        to: account.email,
+        subject: "RS Navigation - Reset Password",
+        text: message
+    };
+    
+    gmail.sendMail(mailOptions, (err, info) => {
+        if (err) { return callback(err); }
+        logger.info(`Reset password email sent to ${account.email}] - ${info.response}`);
+        callback(null);
+    });
+}
