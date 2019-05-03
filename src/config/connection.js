e5c8d7bade2e9d27ccc9cc19e77d83ffc5c52af5
alevfalse <exactfalse@gmail.com>
2019-05-04 00:34:22 +0800

    use logger, set useCreateIndex and useFindAndModify to true for mongoose

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,5 +1,6 @@
 const database = require('./database');
 const mongoose = require('mongoose');
+const logger = require('../config/logger');
 
 const mode = process.env.MODE;
 
@@ -10,16 +11,21 @@ switch (mode)
     case 'dev':   uri = database.devURI;   break;
     case 'prod':  uri = database.prodURI;  break;
     case 'local': uri = database.localURI; break;
-    default: return console.error('Invalid MODE environment variable value.');
+    default: {
+        logger.error('Invalid MODE environment variable value.');
+        process.exit(1);
+    }
 }
 
 // TODO: https://mongoosejs.com/docs/guide.html#options
 
-//use native findOneAndUpdate() rather than deprecated findAndModify()
-mongoose.set('useFindAndModify', false);
-
 // create a default mongoose connection
-const connection = mongoose.connect(uri, { useNewUrlParser: true });
-console.log(`Connecting to database: ${uri}`);
+const connection = mongoose.connect(uri, { 
+    useNewUrlParser: true, 
+    useCreateIndex: true, 
+    useFindAndModify: true 
+});
+
+logger.info(`Connecting to database: ${uri}`);
 
 module.exports = connection;
