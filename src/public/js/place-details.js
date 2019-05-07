e2519406a9854ec0c05763ad887003596e34c163
alevfalse <exactfalse@gmail.com>
2019-05-08 00:22:56 +0800

    fade in and check if place.reviews is not undefined before checking length

--- src/public/js/place-details.js
+++ src/public/js/place-details.js
@@ -1,3 +1,7 @@
+$(document).ready(() => {
+    $("main").animate({ opacity: 1 }, 1000) // fade-in
+});
+
 $("#report-button, #cancel-report-button").click(function() {
     $("#report-form").collapse("toggle");
     $("#place-title").collapse("toggle");
