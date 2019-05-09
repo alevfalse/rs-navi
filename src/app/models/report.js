0a94a17b48ec2fd70b976d76b2463d7284079782
Alexander Paul L. Mari�as <201503009@iacademy.loc>
2019-05-10 06:23:31 +0800

    remove user and review as report target models

--- src/app/models/report.js
+++ src/app/models/report.js
@@ -6,20 +6,20 @@ const ReportSchema = new mongoose.Schema({
     createdAt: { type: Date, default: new Date() },
     status: { type: Number, default: 0 },
     author: { type: String, ref: 'User', required: true },
-    reason: { type: String, required: true },
-    target: { 
-        type: String, // id of target place/user/review
-        refPath: 'targetModel',
-        required: true
-    },
-    targetModel: {
-        type: String,
-        enum: ['User', 'Place', 'Review'],
-        required: true
-    }
+    target: { type: String, ref: 'Place', required: true },
+    type: { type: Number,  required: true },
+    comment: { type: String, required: true }
 });
 
-/**
+/*
+report type for place
+0 - Not available
+1 - Placeowner not responding
+2 - Incorrect information
+3 - Duplicate listing
+4 - Fake or spam
+5 - Other
+
 status
 0 - unresolved
 1 - resolved
