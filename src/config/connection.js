87159b24c6994644e6afd344272ae5b4bc637c26
alevfalse <exactfalse@gmail.com>
2019-05-12 14:01:43 +0800

    define database uri at database.js module

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,31 +1,16 @@
-const database = require('./database');
+const dbURI = require('./database');
 const mongoose = require('mongoose');
 const logger = require('../config/logger');
-
-const mode = process.env.MODE;
-
-let uri;
-
-switch (mode)
-{
-    case 'dev':   uri = database.devURI;   break;
-    case 'prod':  uri = database.prodURI;  break;
-    case 'local': uri = database.localURI; break;
-    default: {
-        logger.error('Invalid MODE environment variable value.');
-        process.exit(1);
-    }
-}
 
 // TODO: https://mongoosejs.com/docs/guide.html#options
 
 // create a default mongoose connection
-const connection = mongoose.connect(uri, { 
+const connection = mongoose.connect(dbURI, { 
     useNewUrlParser: true, 
     useCreateIndex: true, 
     useFindAndModify: true 
 });
 
-logger.info(`Connecting to database: ${uri}`);
+logger.info(`Connecting to database: ${dbURI}`);
 
 module.exports = connection;
--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,16 @@
-module.exports = {
-    prodURI:  `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@rs-navi-zp6p4.mongodb.net/rs-navi?retryWrites=true`,
-    devURI:   `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@rs-navi-zp6p4.mongodb.net/rs-navi-dev?retryWrites=true`,
-    localURI: `mongodb://localhost:27017/rs-navi-local`
+const logger = require('../config/logger');
+
+// get the database URI based on the MODE env variable
+let uri = '';
+
+switch(process.env.MODE)
+{
+    case 'prod': uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@rs-navi-zp6p4.mongodb.net/rs-navi?retryWrites=true`; break;
+    case 'dev' : uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@rs-navi-zp6p4.mongodb.net/rs-navi-dev?retryWrites=true`; break;
+    case 'local': uri = `mongodb://localhost:27017/rs-navi-local`; break;
+    default:  
+        logger.error(`Database URI: Environment variable MODE contains an invalid value.`);
+        process.exit(1);
 }
+
+module.exports = uri;
