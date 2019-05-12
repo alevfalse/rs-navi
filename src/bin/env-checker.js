8b859fc7acd5b46fd2efc65a68eae38ad8b43642
alevfalse <exactfalse@gmail.com>
2019-05-12 14:01:06 +0800

    use undefined funciton

--- src/bin/env-checker.js
+++ src/bin/env-checker.js
@@ -1,13 +1,17 @@
 const logger = require('../config/logger');
 
+function undef(x) {
+    return typeof x === 'undefined';
+}
+
 module.exports = function() {
     let valid = true;
 
-    if (!process.env.PORT) {
+    if (undef(process.env.PORT)) {
         logger.warn(`The PORT environment variable is not set in your environment file (.env).`);
     }
 
-    if (!process.env.MODE) {
+    if (undef(process.env.MODE)) {
         valid = false;
         logger.error(`The MODE environment variable is not set in your environment file (.env). Set it to 'dev' or 'local' during development, testing or staging.\n`
             + `Set it to 'prod' during production or official release of the application.`);
@@ -16,32 +20,32 @@ module.exports = function() {
         logger.error(`The MODE environment variable is set to an invalid value. Valid values are 'dev', 'prod', or 'local' without the single quotes.`);
     }
 
-    if (!process.env.SESSION_SECRET) {
+    if (undef(process.env.SESSION_SECRET)) {
         valid = false;
         logger.error(`The SESSION_SECRET environment variable is not set in your environment file (.env).`);
     }
 
-    if (!process.env.ADMIN_SESSION_SECRET) {
+    if (undef(process.env.ADMIN_SESSION_SECRET)) {
         valid = false;
         logger.error(`The ADMIN_SESSION_SECRET environment variable is not set in your environment file (.env).`);
     }
 
-    if (!process.env.HASH_CODE_SALT) {
+    if (undef(process.env.HASH_CODE_SALT)) {
         valid = false;
         logger.error(`The HASH_CODE_SALT environment variable is not set in your environment file (.env).`);
     }
 
-    if (!process.env.MONGODB_USERNAME) {
+    if (undef(process.env.MONGODB_USERNAME)) {
         valid = false;
         logger.error(`The MONGODB_USERNAME environment variable is not set in your environment file (.env).`);
     }
 
-    if (!process.env.MONGODB_PASSWORD) {
+    if (undef(process.env.MONGODB_PASSWORD)) {
         valid = false;
         logger.error(`The MONGODB_PASSWORD environment variable is not set in your environment file (.env).`);
     }
 
-    if (!process.env.RSNAVI_GMAIL_PASSWORD) {
+    if (undef(process.env.RSNAVI_GMAIL_PASSWORD)) {
         valid = false;
         logger.error(`The RSNAVI_GMAIL_PASSWORD environment variable is not set in your environment file (.env).`);
     }
